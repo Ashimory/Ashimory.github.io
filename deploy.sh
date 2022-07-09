b15560044d34a61fac1d9ce083b413c17ee4f00b
Ashimory <47056302+Ashimory@users.noreply.github.com>
2022-07-10 02:45:52 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,11 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-git init
-git checkout -b master
-git add -A
+git add -A -f
 git commit -m 'deploy'
 
-git remote add origin http://github.com/ashimory/ashimory.github.io.git
 
-git push git push -u origin master
+git subtree push -u origin gh-pages
 
 
 cd -
