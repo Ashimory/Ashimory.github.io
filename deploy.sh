6b45e3aec7823497f853ad85a11bca4fbe408ecc
Ashimory <47056302+Ashimory@users.noreply.github.com>
2022-07-10 17:30:42 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,14 @@ set -e
 npm run build
 
 
+# git add . 
+# git commit -m 'oops messed up setup'
+# git push origin master
+
+
 git add dist -f
 git commit -m 'deploy'
-git subtree push --prefix dist origin gh-pages
+git subtree push -f --prefix dist origin gh-pages
 
 
 # cd -
